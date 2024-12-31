5574ed7c92a69b7a2a0feba58870613dc4eb3f4d
eeisegn <eeisegn@gmail.com>
2024-12-31 12:31:24 +0000

    documentation and error checking cleanup

--- src/main/java/com/scanoss/utils/Purl2Url.java
+++ src/main/java/com/scanoss/utils/Purl2Url.java
@@ -1,33 +1,83 @@
+// SPDX-License-Identifier: MIT
+/*
+ * Copyright (c) 2024, SCANOSS
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package com.scanoss.utils;
 
 import com.github.packageurl.PackageURL;
 import lombok.Getter;
-import org.jetbrains.annotations.NotNull;
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.Nullable;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * Converts Package URLs (purls) to their corresponding browsable web URLs for
  * different package management systems and source code repositories.
  */
+@Slf4j
 public class Purl2Url {
-    private static final Logger log = LoggerFactory.getLogger(Purl2Url.class);
 
+    /**
+     * PURL type to URL enum
+     */
     @Getter
     public enum PurlType {
+        /**
+         * GitHub URL
+         */
         GITHUB("github", "https://github.com/%s"),
+        /**
+         * Node URL
+         */
         NPM("npm", "https://www.npmjs.com/package/%s"),
+        /**
+         * Maven Central URL
+         */
         MAVEN("maven", "https://mvnrepository.com/artifact/%s"),
+        /**
+         * Ruby Gems URL
+         */
         GEM("gem", "https://rubygems.org/gems/%s"),
+        /**
+         * Python PyPI URL
+         */
         PYPI("pypi", "https://pypi.org/project/%s"),
+        /**
+         * Golang URL
+         */
         GOLANG("golang", "https://pkg.go.dev/%s"),
+        /**
+         * MS Nuget URL
+         */
         NUGET("nuget", "https://www.nuget.org/packages/%s");
-
 
         private final String type;
         private final String urlPattern;
 
+        /**
+         * Setup PURL type/URL enum
+         *
+         * @param type PURL Type
+         * @param urlPattern URL pattern
+         */
         PurlType(String type, String urlPattern) {
             this.type = type;
             this.urlPattern = urlPattern;
@@ -38,15 +88,16 @@ public class Purl2Url {
      * Checks if the given PackageURL is supported for conversion.
      *
      * @param purl The PackageURL to check
-     * @return true if the PackageURL can be converted to a browsable URL
+     * @return <code>true</code> if the PackageURL can be converted to a browsable URL
      */
-    public static boolean isSupported(@NotNull PackageURL purl) {
+    public static boolean isSupported(@NonNull PackageURL purl) {
         try {
             findPurlType(purl.getType());
             return true;
-        } catch (Exception e) {
-            return false;
+        } catch (RuntimeException e) {
+            log.warn("Failed to find PURL type {} from {}: {}", purl.getType(), purl, e.getLocalizedMessage());
         }
+        return false;
     }
 
     /**
@@ -54,30 +105,34 @@ public class Purl2Url {
      * Returns null if the conversion is not possible.
      *
      * @param purl The PackageURL to convert
-     * @return The browsable web URL or null if conversion fails
+     * @return The browsable web URL or <code>null</code> if conversion fails
      */
     @Nullable
-    public static String convert(@NotNull PackageURL purl) {
+    public static String convert(@NonNull PackageURL purl) {
         try {
             PurlType purlType = findPurlType(purl.getType());
-            String fullName = purl.getNamespace() != null ?
-                    purl.getNamespace() + "/" + purl.getName() :
-                    purl.getName();
+            String nameSpace = purl.getNamespace();
+            String fullName = nameSpace != null ? nameSpace + "/" + purl.getName() : purl.getName();
             return String.format(purlType.getUrlPattern(), fullName);
-        } catch (Exception e) {
-            log.debug("Failed to convert purl to URL: {}", purl, e);
-            return null;
+        } catch (RuntimeException e) {
+            log.debug("Failed to convert purl to URL for {}: {}", purl, e.getLocalizedMessage(), e);
         }
+        return null;
     }
 
-    private static PurlType findPurlType(String type) {
+    /**
+     * Determine if we have a supported PURL Type or not
+     *
+     * @param type PURL type string
+     * @return Supported PURL Type
+     * @throws IllegalArgumentException if type cannot be found on supported list
+     */
+    private static PurlType findPurlType(@NonNull String type) throws IllegalArgumentException {
         for (PurlType purlType : PurlType.values()) {
             if (purlType.getType().equals(type)) {
                 return purlType;
             }
         }
-        throw new IllegalArgumentException(
-                String.format("Unsupported package type: %s", type)
-        );
+        throw new IllegalArgumentException(String.format("Unsupported package type: %s", type));
     }
 }
